e8ee7083c144409e13775c40f64c4e6c0d4ecbc0
AugustLight <125802350+AugustLigh@users.noreply.github.com>
2024-05-18 01:07:47 +0300

    Make code most clearly

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,15 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+F
+src/main/java/App.java,6\7\67512e2f04487cca15d6699055f74a51de1a9eb4
+R
+"src/main/java/org/example/App.java,2\6\268b09d2a5cdab7d148f0ec91f7b3fb51601410d
+Å
+Q.idea/sonarlint/securityhotspotstore/2/6/268b09d2a5cdab7d148f0ec91f7b3fb51601410d,e\5\e522c1cf5ae6402ba0f08e13e9766bd337634aea
+Å
+Q.idea/sonarlint/securityhotspotstore/6/7/67512e2f04487cca15d6699055f74a51de1a9eb4,6\d\6d745d781a776adac3fa77c8278111a3dff5a966
+Å
+Q.idea/sonarlint/securityhotspotstore/4/4/442292b8a7efeabbe4cc176709b833b1792140ec,2\c\2cabb939da04d3de3a200bf2b575690e1a16316d
+w
+G.idea/sonarlint/issuestore/4/4/442292b8a7efeabbe4cc176709b833b1792140ec,8\6\863e0292fd7e9b3e74dafd6cc01f73ba76346482
---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,113 @@
+package org.example;
+
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+public class App {
+    private static final Logger LOGGER = Logger.getLogger(App.class.getName());
+    private static final byte BOX_SIZE = 9;
+    private enum GameState { PLAYING, WINNER_PLAYER, WINNER_COMPUTER, DRAW }
+    private final char[] box = new char[BOX_SIZE];
+    private GameState gameState = GameState.PLAYING;
+
+    public static void main(String[] args) {
+        new App().startGame();
+    }
+
+    private void startGame() {
+        initializeGame();
+        try (Scanner scan = new Scanner(System.in)) {
+            while (gameState == GameState.PLAYING) {
+                displayBoard();
+                playerTurn(scan);
+                if (gameState == GameState.PLAYING) {
+                    computerTurn();
+                    checkGameState();
+                }
+            }
+            displayEndMessage();
+        }
+    }
+
+    private void initializeGame() {
+        for (int i = 0; i < BOX_SIZE; i++) {
+            box[i] = (char) ('1' + i);
+        }
+        LOGGER.info("Enter box number to select. Enjoy!\n");
+    }
+
+    private void displayBoard() {
+        // Я честно не знаю, але у мене {} не працює, як це має бути.
+        LOGGER.info("\n\n " + box[0] + " | " + box[1] + " | " + box[2] + " ");
+        LOGGER.info("-----------");
+        LOGGER.info(" " + box[3] + " | " + box[4] + " | " + box[5] + " ");
+        LOGGER.info("-----------");
+        LOGGER.info(" " + box[6] + " | " + box[7] + " | " + box[8] + " \n");
+    }
+
+    private void playerTurn(Scanner scan) {
+        byte input;
+        while (true) {
+            LOGGER.info("Your turn: ");
+            input = scan.nextByte();
+            if (input > 0 && input <= BOX_SIZE && box[input - 1] != 'X' && box[input - 1] != 'O') {
+                box[input - 1] = 'X';
+                break;
+            } else {
+                LOGGER.info("Invalid input or box already filled. Try again.");
+            }
+        }
+    }
+
+    private void computerTurn() {
+        byte rand;
+        while (true) {
+            rand = (byte) (Math.random() * BOX_SIZE);
+            if (box[rand] != 'X' && box[rand] != 'O') {
+                box[rand] = 'O';
+                LOGGER.info("Computer chose box " + (rand));
+                break;
+            }
+        }
+    }
+
+    private void checkGameState() {
+        if (isWinner('X')) {
+            gameState = GameState.WINNER_PLAYER;
+        } else if (isWinner('O')) {
+            gameState = GameState.WINNER_COMPUTER;
+        } else if (isBoardFull()) {
+            gameState = GameState.DRAW;
+        }
+    }
+
+    private boolean isWinner(char player) {
+        return (box[0] == player && box[1] == player && box[2] == player) ||
+                (box[3] == player && box[4] == player && box[5] == player) ||
+                (box[6] == player && box[7] == player && box[8] == player) ||
+                (box[0] == player && box[3] == player && box[6] == player) ||
+                (box[1] == player && box[4] == player && box[7] == player) ||
+                (box[2] == player && box[5] == player && box[8] == player) ||
+                (box[0] == player && box[4] == player && box[8] == player) ||
+                (box[2] == player && box[4] == player && box[6] == player);
+    }
+
+    private boolean isBoardFull() {
+        for (int i = 0; i < BOX_SIZE; i++) {
+            if (box[i] != 'X' && box[i] != 'O') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void displayEndMessage() {
+        if (gameState == GameState.WINNER_PLAYER) {
+            LOGGER.info("You won the game!\nCreated by Shreyas Saha. Thanks for playing!");
+        } else if (gameState == GameState.WINNER_COMPUTER) {
+            LOGGER.info("You lost the game!\nCreated by Shreyas Saha. Thanks for playing!");
+        } else {
+            LOGGER.info("It's a draw!\nCreated by Shreyas Saha. Thanks for playing!");
+        }
+    }
+}
